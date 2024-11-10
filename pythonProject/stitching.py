a0a57f4e1e4f64cf960f1ecb54e5f4058dc3b88e
Petr B <batek.p92@gmail.com>
2024-11-10 22:14:34 +0100

    Pipeline for taking images and stitching at once.

--- pythonProject/stitching.py
+++ pythonProject/stitching.py
@@ -12,6 +12,7 @@ or scans.
 from __future__ import print_function
 
 import os
+import time
 
 import numpy as np
 import cv2 as cv
@@ -19,63 +20,89 @@ import cv2 as cv
 import argparse
 import sys
 
-modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
-
-parser = argparse.ArgumentParser(prog='stitching.py', description='Stitching sample.')
-parser.add_argument('--mode',
-                    type=int, choices=modes, default=cv.Stitcher_PANORAMA,
-                    help='Determines configuration of stitcher. The default is `PANORAMA` (%d), '
-                         'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
-                         'for stitching materials under affine transformation, such as scans.' % modes)
-parser.add_argument('--output', default='result.jpg',
-                    help='Resulting image. The default is `result.jpg`.')
-parser.add_argument('img', nargs='+', help='input images')
-
-__doc__ += '\n' + parser.format_help()
+from camera_calibration import CamIntrinsics
+from pythonProject.camera_ezviz import CamEzviz
 
 
-def main():
-    args = parser.parse_args()
+def undistort(in_images):
 
     # Load camera parameters for image rectification
-    mtx = np.loadtxt("../Ezviz_C6N/C6N_intrinsics.txt")
-    dist = np.loadtxt("../Ezviz_C6N/C6N_distortions.txt")
-    # read input images
-    imgs = []
-    for img_name in args.img:
-        img = cv.imread(img_name)
-        if img is None:
-            print("can't read image " + img_name)
-            sys.exit(-1)
+    cam_instrinsics = CamIntrinsics("../Ezviz_C6N")
+
+    # Undistort images
+    out_images = []
+    for img in in_images:
         # Image undistortion
-        h, w = img.shape[:2]
-        newcameramtx, roi = cv.getOptimalNewCameraMatrix(mtx, dist, (w, h), 1, (w, h))
+        dst = cam_instrinsics.undistort(img)
 
-        # undistort
-        dst = cv.undistort(img, mtx, dist, None, newcameramtx)
+        out_images.append(dst)
 
-        # crop the image
-        x, y, w, h = roi
-        dst = dst[y:y + h, x:x + w]
+    return out_images
 
-        imgs.append(dst)
 
+def stitch(in_images, mode):
     # ![stitching]
-    stitcher = cv.Stitcher.create(args.mode)
-    status, pano = stitcher.stitch(imgs)
+    stitcher = cv.Stitcher.create(mode)
+    status, pano = stitcher.stitch(in_images)
 
     if status != cv.Stitcher_OK:
         print("Can't stitch images, error code = %d" % status)
         sys.exit(-1)
-    # ![stitching]
 
-    cv.imwrite(args.output, pano)
-    print("stitching completed successfully. %s saved!" % args.output)
+    print("Stitching completed successfully.")
+    cv.imshow("Stitched image", pano)
 
-    print('Done')
+    return pano
 
 
 if __name__ == '__main__':
+    modes = (cv.Stitcher_PANORAMA, cv.Stitcher_SCANS)
+
+    parser = argparse.ArgumentParser(prog='stitching.py', description='Stitching sample.')
+    parser.add_argument('--mode',
+                        type=int, choices=modes, default=cv.Stitcher_PANORAMA,
+                        help='Determines configuration of stitcher. The default is `PANORAMA` (%d), '
+                             'mode suitable for creating photo panoramas. Option `SCANS` (%d) is suitable '
+                             'for stitching materials under affine transformation, such as scans.' % modes)
+    parser.add_argument('--output',
+                        help='Save resulting image as.')
+    parser.add_argument('--img', nargs='+', help='input images')
+
+    __doc__ += '\n' + parser.format_help()
     print(__doc__)
-    main()
+
+    t1 = time.time_ns()
+    args = parser.parse_args()
+
+    if args.img:    # Work with supplied images
+        # read input images
+        in_images = []
+        for img_name in args.img:
+            img = cv.imread(img_name)
+            if img is None:
+                print("can't read image " + img_name)
+                sys.exit(-1)
+            in_images.append(img)
+        preprocessed_imgs = undistort(in_images)
+    else:   # No images supplied. Take scan with camera.
+        # Load environment variables
+        EZVIZ_USERNAME = os.getenv("EZVIZ_USERNAME")
+        EZVIZ_PASSWORD = os.getenv("EZVIZ_PASSWORD")
+
+        RTSP_URL = os.getenv("RTSP_URL")
+
+        cam = CamEzviz(RTSP_URL, EZVIZ_USERNAME, EZVIZ_PASSWORD)
+        t1 = time.time_ns()
+        images = cam.scan(undistort=False)
+        preprocessed_imgs = cam.undistort(images)
+        cam.close()
+
+    stitched = stitch(in_images=preprocessed_imgs, mode=args.mode)
+
+    if args.output:
+        cv.imwrite(args.output, stitched)
+
+    print("time: " + str(1e-9 * (time.time_ns() - t1)))
+
+    cv.waitKey(0)
     cv.destroyAllWindows()
